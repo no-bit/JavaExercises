843b4004a3d6ceca2857ed8f87b15e6d24dcc307
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-11-22 15:32:12 +0300

    ConvertToCentimeters, java the complete java developer course.

---
+++ src/ConvertToCentimeters.java
@@ -0,0 +1,12 @@
+public class ConvertToCentimeters {
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(5, 10));
+        System.out.println(convertToCentimeters(70));
+    }
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+    public static double convertToCentimeters(int feet, int inches){
+        return (feet * 12 + inches) * 2.54;
+    }
+}
