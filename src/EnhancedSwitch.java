1d9f33273bf0ef7ba3bdbdf277cb2e6092b4a20f
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-11-24 05:25:47 +0300

    EnhancedSwitch, java the complete java developer course.

---
+++ src/EnhancedSwitch.java
@@ -0,0 +1,15 @@
+public class EnhancedSwitch {
+        public static void main(String[] args) {
+            switchChar('D');
+        }
+        public static void switchChar(char switchValue) {
+            switch (switchValue) {
+                case 'A' -> System.out.println("Able");
+                case 'B' -> System.out.println("Baker");
+                case 'C' -> System.out.println("Charlie");
+                case 'D' -> System.out.println("Dog");
+                case 'E' -> System.out.println("Easy");
+                default -> System.out.println("Message not found");
+            }
+        }
+}
--- src/TraditionalSwitchChallenge.java
+++ src/TraditionalSwitchChallenge.java
@@ -4,24 +4,13 @@ public class TraditionalSwitchChallenge {
      switchChar('D');
     }
     public static void switchChar(char switchValue) {
-        switch (switchValue){
-            case 'A':
-                System.out.println("Able");
-                break;
-            case 'B':
-                System.out.println("Baker");
-                break;
-            case 'C':
-                System.out.println("Charlie");
-                break;
-            case 'D':
-                System.out.println("Dog");
-                break;
-            case 'E':
-                System.out.println("Easy");
-                break;
-            default:
-                System.out.println("Message not found");
+        switch (switchValue) {
+            case 'A' -> System.out.println("Able");
+            case 'B' -> System.out.println("Baker");
+            case 'C' -> System.out.println("Charlie");
+            case 'D' -> System.out.println("Dog");
+            case 'E' -> System.out.println("Easy");
+            default -> System.out.println("Message not found");
         }
     }
 }
