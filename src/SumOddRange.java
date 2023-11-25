ba5af18b19ef56dc24b6228eed4d03c96bd957f5
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-11-25 22:18:47 +0300

    SumOddRange, java the complete java developer course.

---
+++ src/SumOddRange.java
@@ -0,0 +1,16 @@
+public class SumOddRange {
+    public static boolean isOdd(int number){
+        if (number <= 0){
+            return false;
+        }return number % 2 != 0;
+    }
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        if (end < start || end < 0 || start < 0)
+            return -1;
+        for (int i = start; i <= end; i++){
+            if (isOdd(i))
+                sum += i;
+        }return sum;
+    }
+}
