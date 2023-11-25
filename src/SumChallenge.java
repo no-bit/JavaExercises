b0d2b1c3acda53e19800da1562addd5fd7518818
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-11-25 20:03:22 +0300

    SumChallenge, java the complete java developer course.

---
+++ src/SumChallenge.java
@@ -0,0 +1,16 @@
+public class SumChallenge {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        for(int i = 1; i <=1000; i++ ){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println(i);
+                count++;
+                sum += i;
+                if (count == 5)
+                    break;
+            }
+        }
+        System.out.println("total sum = " + sum);
+    }
+}
