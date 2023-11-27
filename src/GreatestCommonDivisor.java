119fb53539dabf1d9c25a8b7285d6e2e9486618e
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-11-27 11:12:49 +0300

    GreatestCommonDivisor, java the complete java developer course.

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,11 @@
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        if(first < 10 || second < 10)
+            return -1;
+        for (int i = first; i >= 1; i--){
+            if (first % i == 0 && second % i ==0){
+                return i;
+            }
+        }return 1;
+    }
+}
