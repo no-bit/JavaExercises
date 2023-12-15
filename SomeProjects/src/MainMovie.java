13f00d3881c112cb5a4d6561aff4c86b27dc3aca
aysin <sinanburakfiliz@gmail.com>
2023-12-15 22:02:48 +0300

    Movie, polymorphism.

---
+++ SomeProjects/src/MainMovie.java
@@ -0,0 +1,7 @@
+public class MainMovie {
+    public static void main(String[] args) {
+        Movie movie = Movie.getMovie("Adventure","Star Wars");
+        movie.watchMovie();
+    }
+}
+//.contains(type)
