72f4e809799903ab60361c3fbaff930b8210c24c
SinanBurakFiliz <sinanburakfiliz@Sinan-MacBook-Air.local>
2023-12-20 17:26:01 +0300

    Car and CarMain polymorphism in OOP, java the complete java developer course.

---
+++ src/Car.java
@@ -0,0 +1,94 @@
+public class Car {
+    private boolean engine = true;
+    private int cylinders;
+    private String name;
+    private int wheels = 4;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+    }
+
+    public String startEngine(){
+        return (this.getClass().getName() + " -> startEngine()");
+    }
+
+    public String accelerate(){
+        return (this.getClass().getName() +  " -> accelerate()");
+    }
+
+    public String brake(){
+        return (this.getClass().getName() + " -> brake()");
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Mitsubishi extends Car{
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
+
+class Holden extends Car{
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
+
+class Ford extends Car{
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
