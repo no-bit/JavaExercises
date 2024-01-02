bccda4a0307a81fb3df2bb16af615a6f358659bd
aysin <sinanburakfiliz@gmail.com>
2024-01-02 15:52:22 +0300

    Uncompleted abstract too vague and time-consuming descriptions.

--- MoreProjects/src/Store.java
+++ MoreProjects/src/Store.java
@@ -1,5 +1,9 @@
 public class Store extends ProductForSale {
 
+    public Store(String type, int price, String description) {
+        super(type, price, description);
+    }
+
     @Override
     public void showDetails() {
 
